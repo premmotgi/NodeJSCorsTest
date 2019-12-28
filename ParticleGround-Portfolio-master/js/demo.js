6ee6a4162d1ef92971e562ac97f2ed27513fbec0
premmotgi <55761557+premmotgi@users.noreply.github.com>
2019-12-27 20:45:03 -0500

    Add files via upload
    added backend and fe

---
+++ ParticleGround-Portfolio-master/js/demo.js
@@ -0,0 +1,38 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+// This can be used to set the Particles Effects. Check README for more details!
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#0F29BD',
+    lineColor: '#0CBD37'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+function calCard(){
+
+  window.open("IdentityCard.html")
+
+}
+
+function gotoProjects() {
+  window.open("ProjectInfo.html")
+}
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
